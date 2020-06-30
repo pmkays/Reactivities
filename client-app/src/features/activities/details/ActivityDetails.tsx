47afa3f2464de2c34f7c7771f5bb03dbc5fce835
Paula Kurniawan s3782041 <s3782041@student.rmit.edu.au>
2020-06-30 12:02:16 +1000

    Added form structure

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Card, Image, Button} from 'semantic-ui-react'
 
 export const ActivityDetails = () => {
     return (
-        <div>
-            
-        </div>
+        <Card fluid>
+            <Image src='/assets/placeholder.png' wrapped ui={false} />
+            <Card.Content>
+            <Card.Header>Title</Card.Header>
+            <Card.Meta>
+                <span>Date</span>
+            </Card.Meta>
+            <Card.Description>
+                Description
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group>
+                    <Button basic color = 'blue' content = 'Edit'/>
+                    <Button basic color = 'grey' content = 'Cancel'/>
+                </Button.Group>
+            </Card.Content>
+        </Card>
     )
 }
